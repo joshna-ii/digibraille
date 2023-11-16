a690419ba9d9ccd56c884886d1d8ece5b6908daa
jiyengar-ii <jiyengar@andrew.cmu.edu>
2023-11-16 16:06:13 -0500

    i think just updated readme

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 # digibraille
 
-
-$ chmod +x rpi_arduino.py 
-$ ./rpi_arduino.py 
-
-
-
-
 How to Use Right Now:
 - pip install -r requirements.txt
-- python3 webapp.py
-    - website is 127.0.0.1:5000 (local host)
+- python3 webapp.py or sudo python3 webapp.py on raspberry pi
+    - website is ip address of host device
 
 Structure of Repo:
 - frontend: webapp.py and templates using python library Flask, directly interfaces with main_backend.py
@@ -19,22 +12,17 @@ Structure of Repo:
     - translation.py: uses dict to convert alphabet to braille characters (list of 6 0/1s) for uncontracted braille, loops through again for uncontracted braille, converts to list of location and solenoid movement to be sent
     - rpi_handler.py: interfaces with rpi to host website and send signals for embosser
     - create_database.py: file that webscrapes directionsforme and other websites and outputs info into database in database_output.txt using beautifulsoup
+    - databasexxxx.csv: 
+    - demo.ino: arduino file that is run on the arduino and interacts with rpi_handler.py over serial
 - temporary files:
     - temp_code.py: temporary python code file for testing
     - temp_output.py: print temporary test outputs here
 
 Things to do:
-- figure out how to host website with rpi in rpi_handler.py
-- create database (dict where keywords map to webscraped info) in create_database.py
-- test to see if data structure for accessing database is accessible and accurate in websearch.py
-- string to contracted braille translations in translation.py
-- string to uncontracted braille translations in translation.py
-- braille translation to embosser instructions algorithm in translation.py
-- figure out how to communicate with rpi in rpi_handler.py
-- figure out how database will be handled
+- use rc.local to immediately run webapp.py when rpi is plugged in (headless)
+- webscrape backofthebox.com then create database with it
+- stress unit tests
+- computer vision for printing box instructions
+- rpi to arduino to stepper motor pipeline
 - work on cache system for most recent searches
-
-Things to consider:
-- computer vision printing box instructions option?
-- would people use the print website option from phone
-    - - figure out how best to convert website input html to necessary content in print_link under websearch.py
+- make button for normal vs. jumbo size and grade 1 vs. grade 2 vs. grade 3
--- demo/demo.ino
+++ demo/demo.ino
@@ -26,7 +26,7 @@ void setup() {
 }
 
 void comb0(){
-   
+   Serial.println("arduino sent: 0");
 }
 
 void comb1(){
@@ -34,7 +34,7 @@ void comb1(){
     delay(d);
     digitalWrite(s4, LOW);   
     delay(d);
-  
+    Serial.println("arduino sent: 1");
 }
 
 void comb2(){
@@ -42,17 +42,17 @@ void comb2(){
     delay(d);
     digitalWrite(s3, LOW);   
     delay(d);
-  
+    Serial.println("arduino sent: 2");
 }
+
 void comb3(){
-    Serial.println("so bro");
     digitalWrite(s3, HIGH); 
     digitalWrite(s4, HIGH); 
     delay(d);
     digitalWrite(s3, LOW);   
     digitalWrite(s4, LOW);   
     delay(d);
-  
+    Serial.println("arduino sent: 3");
 }
 
 void comb4(){
@@ -60,7 +60,7 @@ void comb4(){
     delay(d);
     digitalWrite(s2, LOW);   
     delay(d);
-  
+    Serial.println("arduino sent: 4");
 }
 
 void comb5(){
@@ -70,7 +70,7 @@ void comb5(){
     digitalWrite(s4, LOW);   
     digitalWrite(s2, LOW);   
     delay(d);
-  
+    Serial.println("arduino sent: 5");
 }
 
 void comb6(){
@@ -80,7 +80,7 @@ void comb6(){
     digitalWrite(s3, LOW);   
     digitalWrite(s2, LOW);   
     delay(d);
-    
+    Serial.println("arduino sent: 6");
 }
 
 void comb7(){
@@ -92,7 +92,7 @@ void comb7(){
     digitalWrite(s2, LOW); 
     digitalWrite(s4, LOW);   
     delay(d);
-
+    Serial.println("arduino sent: 7");
 }
 
 void comb8(){
@@ -100,7 +100,7 @@ void comb8(){
     delay(d);
     digitalWrite(s1, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 8");
 }
 
 void comb9(){
@@ -110,7 +110,7 @@ void comb9(){
     digitalWrite(s4, LOW);   
     digitalWrite(s1, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 9");
 }
 
 void comb10(){
@@ -120,7 +120,7 @@ void comb10(){
     digitalWrite(s4, LOW);   
     digitalWrite(s1, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 10");
 }
 
 void comb11(){
@@ -132,7 +132,7 @@ void comb11(){
     digitalWrite(s1, LOW); 
     digitalWrite(s3, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 11");
 }
 
 void comb12(){
@@ -142,7 +142,7 @@ void comb12(){
     digitalWrite(s2, LOW);   
     digitalWrite(s1, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 12");
 }
 
 void comb13(){
@@ -154,7 +154,7 @@ void comb13(){
     digitalWrite(s1, LOW); 
     digitalWrite(s4, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 13");
 }
 
 void comb14(){
@@ -166,7 +166,7 @@ void comb14(){
     digitalWrite(s1, LOW); 
     digitalWrite(s3, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 14");
 }
 
 void comb15(){
@@ -180,12 +180,12 @@ void comb15(){
     digitalWrite(s3, LOW); 
     digitalWrite(s4, LOW); 
     delay(d);
-  
+    Serial.println("arduino sent: 15");
 }
 
 void combinations(String s){
-   Serial.println("and now");
    //solenoid combinations
+   if (s.equals("0")){ comb0();}
    if (s.equals("1")){ comb1();}
    if (s.equals("2")){ comb2();}
    if (s.equals("3")){ comb3();}
@@ -197,31 +197,28 @@ void combinations(String s){
    if (s.equals("9")){ comb9();}
    if (s.equals("10")){ comb10();}
    if (s.equals("11")){ comb11();}
-       
+   if (s.equals("12")){ comb12();}
+   if (s.equals("13")){ comb13();}
+   if (s.equals("14")){ comb14();}
+   if (s.equals("15")){ comb15();}      
 }
 
 void test(){
    //solenoid combinations
    int pos = 0;
-   Serial.println("here");
-
+   //combinations(command);
    while (pos != -1){
     int nextPos = command.indexOf(' ', pos);
     if (nextPos == -1){
-       //Serial.println(command.substring(pos, nextPos));
        String s = command.substring(pos, nextPos); 
        pos = nextPos; 
        combinations(s); 
     }
     if (nextPos != -1){
-      String s = command.substring(pos, nextPos);
-   //   Serial.println(command.substring(pos, nextPos));
-     // Serial.println(nextPos);
-      //Serial.println(s);
-      pos = nextPos + 1; // Move the position to the character after the space
-      combinations(s); 
+       String s = command.substring(pos, nextPos);
+       pos = nextPos + 1; // Move the position to the character after the space
+       combinations(s); 
     }
-    
    }
 }
 
@@ -229,9 +226,9 @@ void test(){
 void loop() {
   if (Serial.available()){
     command = Serial.readStringUntil('\n'); //this should be the combo that the rpi sends over
+    Serial.print("arduino recieved: ");
     Serial.println(command);
-    //command = Serial.readStringUntil('\n');
-    //command.trim();
+    command.trim();
     if (command.equals("s1_on")){
       digitalWrite(s1, HIGH);  
       delay(d);
