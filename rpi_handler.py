1cd9d616280a0582f18f40ff4e69e62c56569397
joshna-ii <joshna-ii>
2023-11-18 16:11:43 -0500

    full pipeline debugged

--- rpi_handler.py
+++ rpi_handler.py
@@ -3,25 +3,19 @@ import serial
 import time
 
 
-def send_solenoids(combo_list):
+def send_solenoids(combo):
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     
+    combo_list = [combo]
     length = len(combo_list)
     combo_list = ["x"] + combo_list
     count = 0
-    while count < length:
-        for combo in combo_list:
-            if count < length:
-                line = b''
-                while line == b'':
-                    ser.write(bytes(f"{combo} ", 'utf-8'))
-                    line = ser.readline()
-                with open("arduino_output.txt", "a") as f:
-                    f.writelines(f"{line.decode('utf-8').rstrip()}\n")
-                if line[8:12].decode('utf-8') == "sent":
-                    count += 1
-                time.sleep(1)
-
-
-send_solenoids(["3", "5"])
+    for combo in combo_list:
+        line = b''
+        while line == b'':
+            ser.write(bytes(f"{combo} ", 'utf-8'))
+            line = ser.readline()
+        if line[8:12].decode('utf-8') == "sent":
+            count += 1
+        time.sleep(1)
